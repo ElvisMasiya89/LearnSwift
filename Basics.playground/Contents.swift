eac1cbc1cf660e245704e52a8aad32ceb15e96f5
ElvisM89 <80644724+ElvisMasiya89@users.noreply.github.com>
2022-08-19 01:56:51 +0200

    SwiftVariables
    SwiftVariables

---
+++ Basics.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+//Variables
+
+var greeting = "Hello, World"
+print(greeting)
+
+
+var first_name = "Elvis"
+var  last_name = "M89"
+
+print(first_name + last_name)
+
+//Constants
+ let age = 25
+print(age)
---
+++ LearnSwift/LearnSwift/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  LearnSwift
+//
+//  Created by Elvis Masiya on 2022/08/19.
+//
+
+import Foundation
+
+print("Hello, World!")
+
